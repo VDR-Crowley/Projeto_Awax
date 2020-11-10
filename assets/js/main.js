99158c520a3319e6faff0b558f15ada8772e0c95
Vando dos reis <vandodosreis2001@gmail.com>
2020-11-09 23:47:13 -0300

    Adicionando o js no menu tablet

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function abremenu() {
+  
+  if(document.querySelector("nav").style.display == 'flex') {
+    document.querySelector("nav").style.display = 'none'
+
+  } else {
+    document.querySelector("nav").style.display = 'flex'
+    
+  }
+
+}
